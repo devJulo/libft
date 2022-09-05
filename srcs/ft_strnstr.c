186664b4274b0dcc34e07233a7545e3a92ed43fb
Julien FRANCAIS <jfrancai@student.42.fr>
2022-09-05 09:35:07 +0200

    bug fixe: gcc 11.2.0 variable set but not used detection send a warning

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jfrancai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 12:33:04 by jfrancai          #+#    #+#             */
-/*   Updated: 2021/11/25 11:50:34 by jfrancai         ###   ########.fr       */
+/*   Updated: 2022/09/05 09:29:44 by jfrancai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	little_len;
 	int		j;
 
-	little_len = ft_strlen(little);
 	if (!*little)
 		return ((char *)big);
 	while (*big && len > 0)
